f3e81545a263ebc9aa2c2b6ba9be2a9e1f5ffae1
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-29 16:22:10 +0530

    identify what need files

---
+++ project.js
@@ -0,0 +1,9 @@
+// 1. despot some money
+// 2. determine number of lines to bet on
+// 3. collect a bet amount
+// 4. spin the slot machine 
+// 5. check if the user won 
+// 6. give the user their winnings
+// 7. play agin  
+
+ 
