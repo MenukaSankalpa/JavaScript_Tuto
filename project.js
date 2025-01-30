e9406c14950c9e011e001f245105cc4be7a8d381
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-01-30 09:09:46 +0530

    complete 1 step

--- project.js
+++ project.js
@@ -1,23 +1,31 @@
 // 1. despot some money
 // 2. determine number of lines to bet on
 // 3. collect a bet amount
-// 4. spin the slot machine 
-// 5. check if the user won 
+// 4. spin the slot machine
+// 5. check if the user won
 // 6. give the user their winnings
-// 7. play agin  
+// 7. play agin
 
 //function deposit() {
-//  return  
+//  return
 //
 //}
-//deposit() this equl to const 
-
+//deposit() this equl to const
 
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
-  const depositAmount = prompt("Enter a deposit amount: ");
+  while (true) {
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
 
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again.");
+    } else {
+      return numberDepositAmount;
+    }
+  }
 };
 
-deposit();
+const depositAmount = deposit();
+console.log(depositAmount);
